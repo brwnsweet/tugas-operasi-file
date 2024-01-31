0e002ee70fa2d60bbf91cbe918fbf22cf8bac918
nuril izza <141518206+brwnsweet@users.noreply.github.com>
2024-01-31 09:05:56 +0700

    yes

--- operasi_file.py
+++ operasi_file.py
@@ -1,13 +1,31 @@
-print("SELAMAT DATANG DI PESAN RAHASIA")
-print("*******************************")
-file_pesan = (open("pesan.txt","r+"))
-teks = file_pesan.read()
-print (teks)
+def baca_file(nama_file):
+    try:
+        with open(nama_file, 'r') as file:
+            isi = file.read()
+            print("Isi dari file", nama_file, ":")
+            print(isi)
+    except FileNotFoundError:
+        print("File not found")
 
-nama = input("Nama : ")
-pesan = input("Pesan : ")
-harapan = input("Harapan : ")
-text = "\nNama : {}\nPesan : {}\nHarapan : {}\n".format(nama, pesan, harapan)
-file_pesan.write(text)
-file_pesan.close()
-print("******************************")
+def tulis_file(nama_file, teks):
+    with open(nama_file, 'w') as f:
+        f.write(teks)
+    print("File", nama_file, "telah berhasil ditulis.")
+
+if __name__ == "__main__":
+    # Nama file yang akan dibaca dan ditulis
+    nama_file = "pesan.txt"
+
+    # Isi teks yang akan ditulis ke dalam file
+    isi_teks = """Starts to make its way to me
+The playful conversation starts
+Counter all your quick remarks
+Like passing notes in secrecy
+And it was enchanting to meet you
+I was enchanted to meet you"""
+
+    # Tulis teks ke dalam file
+    tulis_file(nama_file, isi_teks)
+
+    # Baca teks dari file
+    baca_file(nama_file)
